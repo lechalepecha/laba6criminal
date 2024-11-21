6162d6f0d9c09daf2d5da1fe038c8f379ed44f39
lechalepecha <aleksejserakov45@gmail.com>
2024-11-21 19:32:06 +0700

    CrimeAdapter for RecyclerView

--- app/src/main/java/com/example/laba6criminal/CrimeListFragment.kt
+++ app/src/main/java/com/example/laba6criminal/CrimeListFragment.kt
@@ -14,6 +14,8 @@ import androidx.recyclerview.widget.RecyclerView
 private const val TAG = "CrimeListFragment"
 class CrimeListFragment: Fragment() {
     private lateinit var crimeRecyclerView : RecyclerView
+    private var adapter: CrimeAdapter? = null
+
     private val crimeListViewModel : CrimeListViewModel by lazy { ViewModelProviders.of(this).get(CrimeListViewModel::class.java) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,12 +32,35 @@ class CrimeListFragment: Fragment() {
             inflater.inflate(R.layout.fragment_crime_list, container, false)
         crimeRecyclerView = view.findViewById(R.id.crime_recycler_view) as RecyclerView
         crimeRecyclerView.layoutManager = LinearLayoutManager(context)
+
+        updateUI()
         return view
     }
 
     private inner class CrimeHolder(view: View) : RecyclerView.ViewHolder(view) {
         val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
         val dateTextView: TextView = itemView.findViewById(R.id.crime_date)
+    }
+
+    private fun updateUI() {
+        val crimes = crimeListViewModel.crimes
+        adapter = CrimeAdapter(crimes)
+        crimeRecyclerView.adapter = adapter
+    }
+
+    private inner class CrimeAdapter(var crimes: List<Crime>) : RecyclerView.Adapter<CrimeHolder>() {
+        override fun onCreateViewHolder(parent : ViewGroup, viewType: Int) : CrimeHolder {
+            val view = layoutInflater.inflate(R.layout.list_item_crime, parent, false)
+            return CrimeHolder(view)
+        }
+        override fun getItemCount() = crimes.size
+        override fun onBindViewHolder(holder : CrimeHolder, position: Int) {
+            val crime = crimes[position]
+            holder.apply {
+                titleTextView.text = crime.title
+                dateTextView.text = crime.date.toString()
+            }
+        }
     }
 
     companion object{
