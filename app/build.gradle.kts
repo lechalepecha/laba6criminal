5d3b95076bd6c3a385b5c341be89c77288b7cd2e
lechalepecha <aleksejserakov45@gmail.com>
2024-11-21 19:19:46 +0700

    RecycleView

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation("androidx.lifecycle:lifecycle-extensions:2.0.0")
+    implementation("androidx.recyclerview:recyclerview:1.0.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
