45d004e352ee2c7238a2ce4195bb3c51c8613495
lechalepecha <aleksejserakov45@gmail.com>
2024-11-21 20:37:24 +0700

    request testing

--- app/src/main/java/com/example/laba6criminal/CrimeListViewModel.kt
+++ app/src/main/java/com/example/laba6criminal/CrimeListViewModel.kt
@@ -3,14 +3,6 @@ package com.bignerdranch.android.laba6criminal
 import androidx.lifecycle.ViewModel
 
 class CrimeListViewModel: ViewModel() {
-    val crimes = mutableListOf<Crime>()
-    init {
-        for( i in 0..100)
-        {
-            val crime = Crime()
-            crime.title = "Дело #$i"
-            crime.isSolved = i % 2 == 0
-            crimes += crime
-        }
-    }
+    private val crimeRepository = CrimeRepository.get()
+    val crimes = crimeRepository.getCrimes()
 }
